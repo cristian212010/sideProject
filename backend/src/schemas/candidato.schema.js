fe1243225b3a3dd728d481369880846e2b307ae3
cristian212010 <cristiancamilo212010@gmail.com>
2023-12-14 14:21:06 -0500

    feat: :sparkles: CRUD candidato
    se creo el CRUD de candidato con las respectivas validaciones

---
+++ backend/src/schemas/candidato.schema.js
@@ -0,0 +1,31 @@
+import z from 'zod';
+
+const candidatoSchema = z.object({
+    id_usuario_fk: z.number({
+        required_error: 'id_usuario_fk es requerido',
+        invalid_type_error: 'id_usuario_fk debe ser un number'
+    }).int().positive(),
+    id_especialidad_fk: z.number({
+        required_error: 'id_especialidad_fk es requerido',
+        invalid_type_error: 'id_especialidad_fk debe ser un number'
+    }).int().positive(),
+    avatar: z.string({
+        required_error: 'avatar es requerido',
+        invalid_type_error: 'avatar debe ser un string'
+    }).min(1).max(50),
+    disponibilidad_viajar: z.boolean({
+        required_error: 'disponibilidad_viajar es requerido',
+        invalid_type_error: 'disponibilidad_viajar debe ser un booleano'
+    }),
+    descripcion: z.string({
+        invalid_type_error: 'descripcion debe ser un string'
+    }).nullable(),
+});
+
+export function validateCandidato(input) {
+    return candidatoSchema.safeParse(input);
+};
+
+export function validatePartialCandidato(input) {
+    return candidatoSchema.partial().safeParse(input);
+}
--- backend/src/server/server.js
+++ backend/src/server/server.js
@@ -5,10 +5,12 @@ import { create_tipo_documento_router } from '../routes/tipo_documento.routes.js
 import { create_rol_router } from '../routes/rol.routes.js';
 import { create_especialidad_router } from '../routes/especialidad.routes.js';
 import { create_usuario_router } from '../routes/usuario.routes.js';
+import { create_candidato_router } from '../routes/candidato.routes.js';
 import Tipo_documento_model from '../models/tipo_documento.models.js';
 import Rol_model from '../models/rol.models.js';
 import Especialidad_model from '../models/especialidad.models.js';
 import Usuario_model from '../models/usuario.models.js';
+import Candidato_model from '../models/candidato.models.js';
 
 class Server{
 
@@ -20,7 +22,8 @@ class Server{
             tipo_documento: '/api/tipo_documento',
             rol: '/api/rol',
             especialidad: '/api/especialidad',
-            usuario: '/api/usuario'
+            usuario: '/api/usuario',
+            candidato: '/api/candidato'
         };
         this.middlewares();
         this.routes();
@@ -36,7 +39,8 @@ class Server{
         this.app.use(this.paths.tipo_documento, create_tipo_documento_router({tipo_documento_model: Tipo_documento_model})),
         this.app.use(this.paths.rol, create_rol_router({rol_model: Rol_model})),
         this.app.use(this.paths.especialidad, create_especialidad_router({especialidad_model: Especialidad_model})),
-        this.app.use(this.paths.usuario, create_usuario_router({usuario_model: Usuario_model}))
+        this.app.use(this.paths.usuario, create_usuario_router({usuario_model: Usuario_model})),
+        this.app.use(this.paths.candidato, create_candidato_router({candidato_model: Candidato_model}))
     }
 
     listen(){
