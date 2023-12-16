2b6909dc854ae819d5bbcffa4797c8bb826c67f5
cristian212010 <cristiancamilo212010@gmail.com>
2023-12-16 17:05:43 -0500

    feat: :sparkles: se agrego info del usuario en el navbar
    se agrego el nombre del usuario en el navbar y tambien la foto pero tiene problemas

--- frontend/src/components/card.jsx
+++ frontend/src/components/card.jsx
@@ -7,12 +7,12 @@ const Card = ({ title, description, imageUrl }) => {
   const respuestaUsuario = 'si';
 
   return (
-    <div class="card">
-      <div class="image-container">
+    <div className="card">
+      <div className="image-container">
 
-        <label class="ui-bookmark">
+        <label className="ui-bookmark">
             <input type="checkbox"/>
-            <div class="bookmark">
+            <div className="bookmark">
                 <svg viewBox="0 0 32 32">
                     <g>
                         <path d="M27 4v27a1 1 0 0 1-1.625.781L16 24.281l-9.375 7.5A1 1 0 0 1 5 31V4a4 4 0 0 1 4-4h14a4 4 0 0 1 4 4z"></path>
@@ -21,10 +21,10 @@ const Card = ({ title, description, imageUrl }) => {
             </div>
         </label>  
                               
-          <div class="price">Jr.</div>
+          <div className="price">Jr.</div>
       </div>
 
-      <div class="content">
+      <div className="content">
         <div className='name_full'>
             <div className='nombre'> 
               <label>Nombre</label>
@@ -49,8 +49,8 @@ const Card = ({ title, description, imageUrl }) => {
         </div>
       </div>
 
-      <div class="button-container">
-          <button class="buy-button button">Buy Now</button>
+      <div className="button-container">
+          <button className="buy-button button">Buy Now</button>
       </div>
   </div>
   );
