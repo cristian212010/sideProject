ebd8e2ad147baba5352dc3ae05cb9c9ffc81164d
S3bastianBernal <sebas201154@gmail.com>
2023-12-18 01:09:21 -0500

    feat: :sparkles: se creo la primera parte de el post de candidatos

--- frontend/src/pages/perfil.jsx
+++ frontend/src/pages/perfil.jsx
@@ -1,33 +1,57 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import "../assets/css/perfil.css"
 import '../assets/css/buttonRadio.css';
 import candidato from "../assets/img/avatar.jpg"
 import Navbar from "../components/navbar";
+import axios from 'axios'
 
 
 const Perfil = () => {
+
 
     const user_id = localStorage.getItem("id_usuario")
 
     const [isChecked, setIsChecked] = useState(false);
     const [texto, setTexto] = useState('');
+    const [nivelIngles, setNivelIngles] = useState([]);
+    const [especialidad, setEspecialidad] = useState([]);
+    const [tecnologia, setTecnologia] = useState([]);
 
-    const opciones = ['NodeJs', 'AWS', 'Php'];
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        axios.get('http://localhost:6996/api/nivel_ingles', {
+          headers: {
+            'x-api-token-jwt': token,
+          },
+        })
+        .then((response) => {
+          setNivelIngles(response.data);
+        })
+
+        axios.get('http://localhost:6996/api/especialidad')
+        .then((response) => {
+          setEspecialidad(response.data);
+        })
+
+        axios.get('http://localhost:6996/api/tecnologia')
+        .then((response) => {
+          setTecnologia(response.data);
+        })
+
+      },[])
+
 
   // Estado local para realizar un seguimiento de las opciones seleccionadas
   const [seleccionados, setSeleccionados] = useState([]);
-
-  // Función para manejar cambios en la selección
-  const handleSelect = (opcion) => {
-    // Si la opción ya está seleccionada, quítala; de lo contrario, agrégala
-    if (seleccionados.includes(opcion)) {
-      setSeleccionados(seleccionados.filter((item) => item !== opcion));
+  const handleSelect = (index) => {
+    if (seleccionados.includes(index)) {
+      setSeleccionados(seleccionados.filter((item) => item !== index));
     } else {
-      setSeleccionados([...seleccionados, opcion]);
+      setSeleccionados([...seleccionados, index]);
     }
-    }
+  };
 
-    console.log(typeof seleccionados);
 
     const handleCheckboxChange = () => {
         setIsChecked(!isChecked);
@@ -35,6 +59,54 @@ const Perfil = () => {
 
     const handleTextoChange = (event) => {
         setTexto(event.target.value);
+        console.log(texto);
+      };
+
+    /* Post Candidato */
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const token = localStorage.getItem('token');
+        
+        const formData = {
+          avatar: "avatar.jpg",
+          id_usuario_fk: Number(user_id),
+          id_especialidad_fk: Number(event.target.especialidad.value),
+          disponibilidad_viajar: isChecked,
+          id_nivel_ingles_fk: Number(event.target.nivel_ingles.value),
+          descripcion: texto,
+        };
+
+        seleccionados.map((datas)=>{
+            const tecnologiadForm = {
+                datas,
+                id_usuario_fk: Number(user_id)
+            }
+            try {
+                const response = axios.post('http://localhost:6996/api/candidato/', tecnologiadForm, {
+                    headers: {
+                    'x-api-token-jwt': token,
+                    }
+                });
+                console.log('Respuesta del servidor:', response.data);
+            } catch (error) {
+                console.error('Error al enviar el formulario:', error);
+            }
+        })
+        
+
+        try {
+            const response = await axios.post('http://localhost:6996/api/candidato/', formData, {
+                headers: {
+                  'x-api-token-jwt': token,
+                }
+            });
+            console.log('Respuesta del servidor:', response.data);
+        } catch (error) {
+            console.error('Error al enviar el formulario:', error);
+        }
+
+
       };
 
       
@@ -52,24 +124,21 @@ const Perfil = () => {
         </div>
 
         <div className="content_form">
-            <form action="">
+            <form onSubmit={handleSubmit}>
             <div className='name_full_form'>
             <input type="hidden" name="" value={user_id} />
             </div>
             <div className='espec_viaje_form'>
             <div className='espec_form'> 
                 <label>Especialidad</label>
-                <select name="Selecciona una opcion" id="">
-                    <option value="">opcion 1</option>
-                    <option value="">opcion 2</option>
-                </select>
-            </div>
-
-            <div className='espec_form'> 
-                <label>Especialidad</label>
-                <select name="Selecciona una opcion" id="">
-                    <option value="">opcion 1</option>
-                    <option value="">opcion 2</option>
+                <select name="especialidad" id="">
+                    {
+                        especialidad.map((data) =>{
+                            return(
+                                <option value={data.id_especialidad}>{data.especialidad}</option>
+                            )
+                        })
+                    }
                 </select>
             </div>
             <div className='Viaje_form'> 
@@ -88,21 +157,35 @@ const Perfil = () => {
                     </div>
             </div>
             </div>
+            <div className="level_ingles">
+            <div className='espec_form'> 
+                <label>Nivel Ingles</label>
+                <select name="nivel_ingles" id="">
+                {
+                        nivelIngles.map((data) =>{
+                            return(
+                                <option value={data.id_nivel_ingles}>{data.nivel_ingles}</option>
+                            )
+                        })
+                    }
+                </select>
+            </div>
+            </div>
             <div className='tegnologias_form'>
-                   <div className='button_radio_container'>
-                    {opciones.map((opcion) => (
-                        <label key={opcion} className="custom-checkbox-label-radio">
-                        <input
-                            type="checkbox"
-                            checked={seleccionados.includes(opcion)}
-                            onChange={() => handleSelect(opcion)}
-                            className="custom-checkbox-radio"
-                        />
-                        <div className="custom-checkbox-square-radio"></div>
-                        {opcion}
-                        </label>
-                    ))}
-                    </div>
+                <div className='button_radio_container'>
+                {tecnologia.map((data, index) => (
+                    <label key={index} className="custom-checkbox-label-radio">
+                    <input
+                        type="checkbox"
+                        checked={seleccionados.includes(index)}
+                        onChange={() => handleSelect(index)}
+                        className="custom-checkbox-radio"
+                    />
+                    <div className="custom-checkbox-square-radio"></div>
+                    {data.tecnologia}
+                    </label>
+                ))}
+                </div>
             </div>
             <div className="descriccion_form">
                 <div>
@@ -111,8 +194,8 @@ const Perfil = () => {
                         id="large-text-input"
                         value={texto}
                         onChange={handleTextoChange}
-                        rows={6} 
-                        cols={37} 
+                        rows={5} 
+                        cols={40} 
                     ></textarea>
             </div>
             </div>
