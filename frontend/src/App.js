f7118eb72c85daf52864901e3671f3c002fa8a23
cristian212010 <cristiancamilo212010@gmail.com>
2023-12-19 14:50:02 -0500

    feat: :lipstick: Estilos inicio

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,11 +5,13 @@ import Candidatos from './pages/candidatos';
 import Login from './pages/login';
 import Usuarios from './pages/usuarios';
 import Perfil from './pages/perfil';
+import Inicio from './pages/inicio';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route exact path='/inicio' component={Inicio}/>
         <Route exact path='/candidatos' component={Candidatos} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/usuarios' component={Usuarios} />
