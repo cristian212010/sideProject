4eec5b59b55e60b20aa6e1ccf95ca3f4995431b6
S3bastianBernal <sebas201154@gmail.com>
2023-12-19 14:16:44 -0500

    feat: :sparkles: se creo la funcionalidad para que se rechazen a los candidatos con informacion no deseada

--- frontend/src/pages/panel_admin.jsx
+++ frontend/src/pages/panel_admin.jsx
@@ -1,17 +1,51 @@
-import React, {useState} from "react";
+import React, {useState,useEffect } from "react";
 import Navbar from "../components/navbar";
 import '../assets/css/panel_admin.css'
 import axios from "axios";
 
 const Panel_Admin = () =>{
     
-    const [APIData, setAPIData] = useState([]);
-
+    const [datosCandidato, setDatosCandidato] = useState([]);
     const [candidatoSeleccionado, setCandidatoSeleccionado] = useState(null);
 
-    const handleVerDatos = (nombre) => {
-        setCandidatoSeleccionado(nombre);
+    useEffect(() => {
+        axios.get('http://localhost:6996/api/candidato/pendientes/')
+          .then(response => {
+            setDatosCandidato(response.data);
+            console.log(response.data);
+          })
+          .catch(error => {
+            console.error('Error al obtener datos del candidato:', error);
+          });
+    }, []);
+
+    const AceptarCandidato = (id) => {
+        console.log(id);  
     };
+
+    const RechazarCandidato = (data) => {
+         const token = localStorage.getItem('token');
+         console.log(data);
+         const mailerData = {
+            nombre_candidato: data.nombre_usuario,
+            email: data.email,
+         }
+         console.log(mailerData);
+         try {
+            const response = axios.post('http://localhost:6996/mailer/rechazo/', {mailerData}, {
+                headers: {
+                'x-api-token-jwt': token,
+                }
+            });
+            console.log('Respuesta del servidor:', response.data);
+        } catch (error) {
+            console.error('Error al enviar el formulario:', error);
+        }   
+    };
+
+    const handleVerDatos = (candidato) => {
+        setCandidatoSeleccionado(candidato);
+      };
 
 
     return (
@@ -20,40 +54,44 @@ const Panel_Admin = () =>{
         <div className="panel_container">
             <div className="panel">
             <div className="titulo_panel">
-                <p>a</p>
-                <p>a</p>
-                <p>a</p>
-                <p>a</p>
-                <p>a</p>
+                <p>Bienvenido a nuestro panel de Administracion</p>   
             </div>
             <div className="cuerpo_panel">
             <div className="cartas_candidatos">
-                <div className="carta_panel_candidatos">
-                <h3>Nombre 1</h3>
-                <button className="btn_panel" onClick={() => handleVerDatos("Nombre 1")}>
+                {datosCandidato.map((candidato, index) => (
+                <div key={index} className="carta_panel_candidatos">
+                    <h3>{candidato.nombre_usuario}</h3>
+                    <button className="btn_panel" onClick={() => handleVerDatos(candidato)}>
                     Ver Datos
-                </button>
+                    </button>
                 </div>
-                <div className="carta_panel_candidatos">
-                <h3>Nombre 2</h3>
-                <button className="btn_panel" onClick={() => handleVerDatos("Nombre 2")}>
-                    Ver Datos
-                </button>
-                </div>
-                <div className="carta_panel_candidatos">
-                <h3>Nombre 3</h3>
-                <button className="btn_panel" onClick={() => handleVerDatos("Nombre 3")}>
-                    Ver Datos
-                </button>
-                </div>
+                ))}
             </div>
             <div className="informacion_candidato">
                 <div className="cuadro_info_panel">
-                {/* Renderiza el contenido basado en la carta seleccionada */}
                 {candidatoSeleccionado && (
                     <div>
-                    <h3>{candidatoSeleccionado}</h3>
-                    {/* Agrega más contenido según sea necesario */}
+                    <div className="contenido">
+                    <h3>Especialidad:{candidatoSeleccionado.nombre_especialidad}</h3>
+                    <p>Nivel Ingles: {candidatoSeleccionado.nivel_de_ingles}</p>
+                    <p>
+                    Disponibilidad para viajar: {' '}
+                    <span
+                        style={{
+                        color: candidatoSeleccionado.disponibilidad_viajar === 0 ? 'red' : 'green',
+                        fontWeight: 'bold',
+                        }}
+                    >
+                        {candidatoSeleccionado.disponibilidad_viajar === 0 ? '✗ No' : '✔ Sí'}
+                    </span>
+                    </p>
+                    <p>Descripcion: {candidatoSeleccionado.descripcion}</p>
+                    <p>Tecnologias: {candidatoSeleccionado.tecnologias}</p>
+                    </div>
+                    <div className="btn_candidatos">
+                        <button className="btn_aceptar_canidatos" onClick={() => AceptarCandidato(candidatoSeleccionado.id_candidato)}>Aceptar</button>
+                        <button className="btn_rechazar_canidatos" onClick={() => RechazarCandidato(candidatoSeleccionado) }>Rechazar</button>
+                    </div>
                     </div>
                 )}
                 </div>
